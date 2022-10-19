71a990bcd2d86ff70755893a126cf0b098f6d71a
Fatalis <connman2400@gmail.com>
2022-10-18 23:27:25 -0500

    local storage just refuses to work despite everything i tried

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # work-scheduler
+This is a application is for keeping track of a 9 to 5 schedule on a daily basis
+you can leave a note for each hour in this time frame. If a section is greyed out, it means that, that hour has past. Red means that its that exact hour currently, and green means the hour hasnt happend yet
+# Photo of app
+<img src="./assets/img/workS.png">
+
+
+
+# Link to deployed application 
--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
 var currentDate = moment().format('MMMM Do YYYY, h:mm:ss a')
+var testEl = $("9am"); 
 $("#currentDay").html(currentDate);
 
 
@@ -39,16 +40,34 @@ if (timeA < currenTime){
     $(this).removeClass("present");
     $(this).removeClass("past");
     $(this).addClass("future");
-}
-
-
-
-
-   
+}   
 })
 }
-letsGo(); 
+$(document).ready(function () {
+     
+    $(".saveBtn").on("click", function () {
+       
+        var text = $(this).parent().attr("id");
+    })})
+
+/*
+    $("#9am .description").val(localStorage.getItem("9am"));
+    $("#10am .description").val(localStorage.getItem("10am"));
+    $("#11am .description").val(localStorage.getItem("11am"));
+    $("#12pm .description").val(localStorage.getItem("12pm"));
+    $("#1pm .description").val(localStorage.getItem("1pm"));
+    $("#2pm .description").val(localStorage.getItem("2pm"));
+    $("#2pm .description").val(localStorage.getItem("3pm"));
+    $("#4pm .description").val(localStorage.getItem("4pm"));
+    $("#5pm .description").val(localStorage.getItem("5pm"));
+  */  
+ 
+    
+    var text = localStorage.getItem("9am");
+     testEl.textContent = text; 
+      
+    letsGo(); 
 
 
-console.log(timeArray);
+
 
